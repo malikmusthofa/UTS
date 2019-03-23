9ff3146b2b4dadae1d77726e0da1b37c9f2d96ac
malikmusthofa <48853720+malikmusthofa@users.noreply.github.com>
2019-03-23 15:42:07 +0700

    Update hello.js

--- soal 4/hello.js
+++ soal 4/hello.js
@@ -1,1 +1,18 @@
-asdjshdja
+main ();
+
+function main(){
+	
+	//create and arrange elements
+	createAndArrangeElements();
+	}
+
+function createAndArrangeElements(){
+	
+	var kotak = document.createElement("textarea");
+	kotak.id = arguments[0];
+	kotak.style.width = "400px"
+	kotak.style.height = "50px";
+	kotak.value = "Hello, Muhammad Malik Musthofa yang ber-NIM 10215045"
+	
+	document.body.append(kotak);
+}
